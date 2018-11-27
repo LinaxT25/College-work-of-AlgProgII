0bf938d1433169c3c452f74eb301e49b63fd129c
LinaxT25 <LinaxT25@users.noreply.github.com>
2018-11-27 12:53:44 -0300

    Create main.c

---
+++ main.c
@@ -0,0 +1,27 @@
+//Date:27/11/2018
+//Função Principal
+#include "interface.h"
+#include "lista_alunos.h"
+
+int main(void)
+{
+  char option;
+  
+  option=interface()
+  option=toupper(option);
+  switch
+  {
+    case 'C':
+      cadastra_aluno(ls_alunos*);
+    case 'B':
+      busca
+    case 'L':
+    case 'A';
+    case 'R';
+    case 'F';
+      break;
+}
+      
+      
+      
+      
