9b0f94602265271ebffc6c1194207ace8f5f76b5
LinaxT25 <LinaxT25@users.noreply.github.com>
2018-11-27 12:18:36 -0300

    Create function cria_lista_alunos()

--- lista_alunos.h
+++ lista_alunos.h
@@ -9,9 +9,18 @@ typedef struct ls_alunos_st
 } ls_alunos;
 
 //Prototipação
-alunos* cria_lista_alunos();
+ls_alunos* cria_lista_alunos();
 void cadastra_aluno(alunos*);
 no* busca_aluno(alunos*, char);
 void cadastra_com_arquivo(alunos*, char);
 void aprovados(alunos*);
 void reprovados(alunos*);
+
+//Estrutura das funções
+ls_alunos* cria_lista_alunos()
+{
+	ls_alunos* lista = (ls_alunos*) malloc(sizeof(ls_alunos));
+	lista->cabeca = (no*) malloc(sizeof(no));
+	lista->cabeca->ant = NULL;
+	lista->cabeça->prox = NULL;
+}
