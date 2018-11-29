fc8ad2df1bbd94d8d76f021184ce5962328dda8a
LinaxT25 <LinaxT25@users.noreply.github.com>
2018-11-29 20:10:18 -0300

    Merge in one archive all structs

--- aluno.h
+++ aluno.h
@@ -1,8 +1,9 @@
 //Data: 26/11/18
-//Estrutura de cada aluno
-#define SIZE 50
+//All structs used in work stay here
+#define SIZE 50 //Used as width of students names
 
- typedef struct aluno_st
+/* Struct with data of students */
+typedef struct aluno_st
 {
 	char Nome[SIZE];
 	unsigned int RA;
@@ -11,3 +12,17 @@
 	float PO;
 	float media;
 } aluno;
+
+/* Struct with no(positions) of list */
+typedef struct no_st
+{
+	aluno* al;
+	struct no_st* ant;
+	struct no_st* prox;
+} no;
+
+/* Struct with head of list */
+typedef struct ls_alunos_st
+{
+	no* cabeca;
+} ls_alunos;
