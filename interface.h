88bbf6bb22750731c2c7e4d44015336eca428c59
wildevn <45374307+wildevn@users.noreply.github.com>
2018-12-03 22:50:21 -0200

    Update interface.h

--- interface.h
+++ interface.h
@@ -4,18 +4,6 @@
 
 void interface()
 {
-    printf("Bem-vindo ao sistema de gerenciamento de aluno ALPHA!\n");
-    printf("\n\n");
-    printf("Qual das seguintes opcoes voce deseja realizar?\n\n");
-    printf("Cadastrar novo aluno.(Pressione C)\n");
-    printf("Buscar Aluno.(Pressione B)\n");
-    printf("Ler arquivo com listagem de alunos.(Pressione L)\n");
-    printf("Gerar arquivo com dados dos alunos aprovados.(Pressione A)\n");
-    printf("Gerar arquivo com dados dos alunos reprovados.(Pressione R)\n");
-    printf("Fechar programa.(Pressione F)\n");
-}
-void interface2()
-{
     printf("\n\n");
     printf("Qual das seguintes opcoes voce deseja realizar?\n\n");
     printf("Cadastrar novo aluno.(Pressione C)\n");
